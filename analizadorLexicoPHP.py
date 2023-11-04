6867ffb227e27b38990a0df6eb66dfb2f86447d9
stefanyfariasm <snfarias@espol.edu.ec>
2023-11-04 11:02:30 -0500

    Palabras reservadas

---
+++ analizadorLexicoPHP.py
@@ -0,0 +1,33 @@
+import ply.lex as lex
+
+#Stefany Farias
+#Palabras reservadas
+reservadas={
+    "and":"AND",
+    "array" : "ARRAY",
+    "break": "BREAK",
+    "callable": "CALLABLE",
+    "class": "CLASS",
+    "const": "CONST",
+    "elseif": "ELSEIF",
+    "else": "ELSE",
+    "for": "FOR",
+    "foreach": "FOREACH",
+    "function": "FUNCTION",
+    "if": "IF",
+    "new": "NEW",
+    "or": "OR",
+    "print" : "PRINT",
+    "private": "PRIVATE",
+    "protected": "PROTECTED",
+    "public": "PUBLIC",
+    "return": "RETURN",
+    "static": "STATIC",
+    "var": "VAR",
+    "while": "WHILE",
+    "xor": "XOR",
+    "SplStack" : "STACK",
+    "push" : "PUSH",
+    "pop" : "POP",
+    "echo": "ECHO",
+}
