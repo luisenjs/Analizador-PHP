eb5be4e557ae42784a3675e17bb6165a78002027
stefanyfariasm <snfarias@espol.edu.ec>
2023-11-13 19:47:42 -0500

    reglas sintacticas para declaracion de variables y funciones

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ def p_instrucciones(p):
             | ingreso
             | estructuras_datos
             | funcion
- 
   '''
 
 #Stefany Farias
@@ -26,3 +25,39 @@ def p_salida_forma2(p):
 
 def p_salida_forma3(p):
   '''salida : PRINT salidas_posibles FIN_LINEA'''
+  
+  
+
+#Stefany Farias
+# 3.1 declaración de variables y funciones
+def p_decl_variable(p):
+  "decl_variable : SIGNO_DOLAR IDENTIFICADOR"
+
+def p_asignacion(p):
+  '''asignacion : decl_variable IGUAL valor FIN_LINEA'''
+
+def p_valor(p):
+  '''valor : datos
+
+  '''
+
+def p_datos(p):
+  '''datos : ENTERO
+          | FLOTANTE
+          | CADENA 
+          | BOOLEANO
+  '''
+
+#Stefany Farias
+# 3.2 declaración de funciones
+def p_funcion(p):
+  '''funcion : FUNCTION IDENTIFICADOR PARENTESIS_ABRE PARENTESIS_CIERRA LLAVE_ABRE instrucciones LLAVE_CIERRA
+  '''
+
+def p_funcion_parametros(p):
+  '''funcion : FUNCTION IDENTIFICADOR PARENTESIS_ABRE parametros PARENTESIS_CIERRA LLAVE_ABRE instrucciones LLAVE_CIERRA'''
+
+def p_parametros(p):
+  '''parametros : SIGNO_DOLAR IDENTIFICADOR
+                  | SIGNO_DOLAR IDENTIFICADOR COMA parametros
+  '''
